6582768d2ec5bbc1ba0f29489ca5dd87251aaa82
Ammarhoussem <38921199+Ammarhoussem@users.noreply.github.com>
2018-05-14 10:33:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
+ client.user.setGame(`تحديت جديد للبوت البوت قيد التطوير .`,'https://www.twitch.tv/v5bz');	
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.on('ready', () => {
     console.log('I am ready!');
 });
 
